f72c4c558ecfeddf7c6e2c84b2fa103fe6a6fc6e
Tim Musgrove <tim.musgrove91@gmail.com>
2017-08-13 15:32:11 -0600

    Understanding basics

--- index.vr.js
+++ index.vr.js
@@ -1,34 +1,14 @@
-import React from 'react';
-import {
-  AppRegistry,
-  asset,
-  Pano,
-  Text,
-  View,
-} from 'react-vr';
+import React, { Component } from 'react';
+import { AppRegistry, asset, Pano, View, Text } from 'react-vr';
 
-export default class Basics extends React.Component {
-  render() {
-    return (
-      <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
-      </View>
-    );
-  }
+export default class Basics extends Component {
+    render() {
+        return (
+            <View>
+                <Pano source={asset('starry-sky.jpg')}></Pano>
+            </View>
+        )
+    }
 };
 
 AppRegistry.registerComponent('Basics', () => Basics);
